bbb4f7a73f7f514f719139ad69b473acf0d79752
Andres <andresm.espinosas15@gmail.com>
2023-07-22 01:22:12 -0500

    feat: added list structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import "./style.css";
+import moreIcon from "./more.svg";
+import reload from "./reload.svg";
+import enter from "./enter.svg";
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+const ToDoListContainer = document.getElementById("list-container");
+const reloadIcon = document.getElementById("reload-img");
+const enterIcon = document.getElementById("enter-icon");
+enterIcon.src = enter;
+reloadIcon.src = reload;
+const todoList = [
+  {
+    id: 1,
+    description: "Starting project structure",
+    completed: false,
+  },
+  {
+    id: 2,
+    description: "Initiate writing JS code",
+    completed: false,
+  },
+  {
+    id: 3,
+    description: "Wash my Dog",
+    completed: true,
+  },
+  {
+    id: 4,
+    description: "Pray ",
+    completed: false,
+  },
+];
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const renderList = (list) => {
+  let innerList = "";
+  if (list.length === 0) {
+    innerList = '<h3 class="list-placeholder">Add your first task!</h3>';
+  } else {
+    const sortedList = list.sort((a, b) => a.id - b.id);
+    sortedList.forEach((task) => {
+      innerList += `
+          <li class="to-do-tasks">
+            <div class="check-box ${task.completed ? "completed" : ""}">${
+        task.completed ? "✓" : ""
+      }</div>
+            <p class="task-description ${
+              task.completed ? "line-through" : ""
+            }">${task.description}</p>
+            <img class="more-logo" src="${moreIcon}"/>
+          </li>
+          `;
+    });
+  }
+  ToDoListContainer.innerHTML = innerList;
+};
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+window.onload = renderList(todoList);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
-   devServer: {
-    static: './dist',
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   optimization: {
-    runtimeChunk: 'single',
-  }
- };
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
